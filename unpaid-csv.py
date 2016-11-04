f30d4cfd31a56e5877771f556cc9b930d434703c
Sotirios Vrachas <sotirios@vrachas.com>
2016-11-04 21:54:08 +0200

    agpl

--- unpaid-csv.py
+++ unpaid-csv.py
@@ -1,3 +1,20 @@
+#    Unpaid CSV. A script for Hackerspace.gr to remind members for their late
+#    subscription fees.
+#    Copyright (C) 2016  Sotirios Vrachas <sotirio@vrachas.net>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from csv import reader
 from datetime import date, datetime
 from string import Template
